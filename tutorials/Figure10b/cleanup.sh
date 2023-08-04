bdc23dff6eb10a766bd579de0988211b80b72949
yuschon <45552484+yuschon@users.noreply.github.com>
2023-08-04 20:54:18 +0800

    Add files via upload

---
+++ tutorials/Figure10b/cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ${0%/*} || exit 1    # run from this directory
+
+# Source tutorial clean functions
+. $WM_PROJECT_DIR/bin/tools/CleanFunctions
+
+#Cleanup script
+rm -r dynamicCode
+cleanTimeDirectories
+rm constant/polyMesh/faces
+rm constant/polyMesh/neighbour
+rm constant/polyMesh/owner
+rm constant/polyMesh/points
+rm LiquidAccumulation.dat
---
+++ tutorials/Figure10c/plot.py
@@ -0,0 +1,18 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+file='LiquidAccumulation.dat'
+a0=np.loadtxt(file)
+xData0=a0[:,0]
+yData0=a0[:,2]
+yData1=np.sqrt(2*xData0*0.8/958.8/4200/(0.5+2.26e6/4200/20))
+
+plt.xlabel('Time/s', size=14)
+plt.ylabel('Interface position/m', size=14)
+plt.plot(xData0, yData1, color='b', linestyle='--', label='Analytical solution')
+plt.plot(xData0, yData0, color='r', linestyle='-', label='mcor=100')
+
+plt.legend(loc='lower right')
+plt.savefig('Stefan.png')
+plt.show()
+
