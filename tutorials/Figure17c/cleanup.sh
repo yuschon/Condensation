e5ece1f6246d988a15facbbd61821c52c1dc1a53
yuschon <45552484+yuschon@users.noreply.github.com>
2023-08-04 20:59:38 +0800

    Add files via upload

---
+++ tutorials/Figure17c/cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Cleanup script
+rm -r processor*
+rm -r dynamicCode
+rm -r 0/*
+cleanTimeDirectories
+foamCleanTutorials
+rm constant/polyMesh/faces
+rm constant/polyMesh/neighbour
+rm constant/polyMesh/owner
+rm constant/polyMesh/points
+rm WallHeatFlux.dat
